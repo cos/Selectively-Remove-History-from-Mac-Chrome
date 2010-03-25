dba8b7e8dc44e3ec9c616ab648910e189aacc6d3
Ron Gejman <rsg2119@columbia.edu>
2010-03-24 21:31:12 -0400

    first commit

---
+++ remove_history.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sqlite3'
+
+keywords = [] #put the keywords you want to remove here. E.g. ["reddit", "digg"]
+
+for db_name in ["History", "Archived History"]
+  f = File.expand_path("~/Library/Application Support/Google/Chrome/Default/#{db_name}")
+  db = SQLite3::Database.open(f)
+  q = keywords.collect {|k| "url LIKE '%#{k}%' or title LIKE '%#{k}%' "}.join(" or ")
+  url_ids = db.execute( "SELECT id FROM urls WHERE #{q}" ).collect {|i| i[0].to_i}
+  next if url_ids.empty?
+  q = url_ids.collect {|id| "id = '#{id}'"}.join(" or ")
+  db.execute(" DELETE FROM urls WHERE #{q}")
+  q = url_ids.collect {|id| "url = '#{id}'"}.join(" or ")
+  db.execute(" DELETE FROM visits WHERE #{q}")
+  
+end
