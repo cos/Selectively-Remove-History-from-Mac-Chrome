5cb8e11a5996c5f2d4179ad2ec6c614fb93e2bf8
cos <cosmin.radoi@gmail.com>
2010-05-25 09:34:55 +0300

    Fixed the 'Expression tree is too large' error

--- remove_history.rb
+++ remove_history.rb
@@ -9,9 +9,9 @@ for db_name in ["History", "Archived History"]
   q = keywords.collect {|k| "url LIKE '%#{k}%' or title LIKE '%#{k}%' "}.join(" or ")
   url_ids = db.execute( "SELECT id FROM urls WHERE #{q}" ).collect {|i| i[0].to_i}
   next if url_ids.empty?
-  q = url_ids.collect {|id| "id = '#{id}'"}.join(" or ")
-  db.execute(" DELETE FROM urls WHERE #{q}")
-  q = url_ids.collect {|id| "url = '#{id}'"}.join(" or ")
-  db.execute(" DELETE FROM visits WHERE #{q}")
+  q = url_ids.collect {|id| "'#{id}'"}.join(", ")
+  db.execute(" DELETE FROM urls WHERE id IN (#{q})")
+  q = url_ids.collect {|id| "'#{id}'"}.join(", ")
+  db.execute(" DELETE FROM visits WHERE url IN (#{q})")
   
 end
